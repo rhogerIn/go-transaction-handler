502865c557e5baafe4f36065e4b166541e80fce4
Rogério Souza <67068084+rhogerIn@users.noreply.github.com>
2024-09-05 08:08:08 -0300

    Added concurrency WATCH, MULTI and EXEC

--- internal/core/transaction.go
+++ internal/core/transaction.go
@@ -1,52 +1,60 @@
 package core
 
 import (
-    "fmt"
-    "strconv"
-    "github.com/go-redis/redis/v8"
-    "context"
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"strconv"
 )
 
 type Transaction struct {
-    Account     string  `json:"account"`
-    TotalAmount float64 `json:"totalAmount"`
-    Mcc         string  `json:"mcc"`
-    Merchant    string  `json:"merchant"`
+	Account     string  `json:"account"`
+	TotalAmount float64 `json:"totalAmount"`
+	Mcc         string  `json:"mcc"`
+	Merchant    string  `json:"merchant"`
 }
 
-// HandleTransaction processes the transaction and updates the balance in Redis
-	func HandleTransaction(ctx context.Context, rdb *redis.Client, transaction Transaction) error {
-    var category string
-    switch transaction.Mcc {
-    case "5411", "5412":
-        category = "FOOD"
-    case "5811", "5812":
-        category = "MEAL"
-    default:
-        category = "CASH"
-    }
+func HandleTransaction(ctx context.Context, rdb *redis.Client, transaction Transaction) error {
+	var category string
+	switch transaction.Mcc {
+	case "5411", "5412":
+		category = "FOOD"
+	case "5811", "5812":
+		category = "MEAL"
+	default:
+		category = "CASH"
+	}
 
-    balanceStr, err := rdb.HGet(ctx, "account:"+transaction.Account, category).Result()
-    if err == redis.Nil {
-        return fmt.Errorf("account or category does not exist")
-    } else if err != nil {
-        return err
-    }
+	accountKey := "account:" + transaction.Account
+	err := rdb.Watch(ctx, func(tx *redis.Tx) error {
+		balanceStr, err := rdb.HGet(ctx, accountKey, category).Result()
+		if err == redis.Nil {
+			return fmt.Errorf("account or category does not exist")
+		} else if err != nil {
+			return err
+		}
 
-    balance, err := strconv.ParseFloat(balanceStr, 64)
-    if err != nil {
-        return err
-    }
+		balance, err := strconv.ParseFloat(balanceStr, 64)
+		if err != nil {
+			return err
+		}
 
-    if balance < transaction.TotalAmount {
-        return fmt.Errorf("insufficient balance")
-    }
+		if balance < transaction.TotalAmount {
+			return fmt.Errorf("insufficient balance")
+		}
 
-    newBalance := balance - transaction.TotalAmount
-    _, err = rdb.HSet(ctx, "account:"+transaction.Account, category, newBalance).Result()
-    if err != nil {
-        return err
-    }
+		_, err = tx.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
+			newBalance := balance - transaction.TotalAmount
+			pipe.HSet(ctx, accountKey, category, newBalance)
+			return nil
+		})
 
-    return nil
+		return err
+	})
+
+	if err != nil {
+		return fmt.Errorf("Transaction failed: %v", err)
+	}
+
+	return nil
 }
