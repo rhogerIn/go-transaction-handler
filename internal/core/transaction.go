f4402e8a2ad20ec2c22a0da77bdb8d15121631c3
Rogério Souza <67068084+rhogerIn@users.noreply.github.com>
2024-09-05 08:30:14 -0300

    style: Isolated some business logic

--- internal/core/transaction.go
+++ internal/core/transaction.go
@@ -14,47 +14,72 @@ type Transaction struct {
 	Merchant    string  `json:"merchant"`
 }
 
-func HandleTransaction(ctx context.Context, rdb *redis.Client, transaction Transaction) error {
-	var category string
-	switch transaction.Mcc {
-	case "5411", "5412":
-		category = "FOOD"
-	case "5811", "5812":
-		category = "MEAL"
-	default:
-		category = "CASH"
-	}
+const (
+	MCCFood1 = "5411"
+	MCCFood2 = "5412"
+	MCCFood3 = "5811"
+	MCCFood4 = "5812"
 
+	ErrAccountNotExists    = "Account or category does not exist"
+	ErrInsufficientBalance = "Insufficient balance"
+)
+
+func HandleTransaction(ctx context.Context, rdb *redis.Client, transaction Transaction) error {
+	category := getMCCMapToCategory(transaction.Mcc)
+	if category == "" {
+		return fmt.Errorf("invalid MCC: %s", transaction.Mcc)
+	}
 	accountKey := "account:" + transaction.Account
 	err := rdb.Watch(ctx, func(tx *redis.Tx) error {
-		balanceStr, err := rdb.HGet(ctx, accountKey, category).Result()
-		if err == redis.Nil {
-			return fmt.Errorf("account or category does not exist")
-		} else if err != nil {
-			return err
-		}
-
-		balance, err := strconv.ParseFloat(balanceStr, 64)
+		balance, err := getBalance(ctx, tx, accountKey, category)
 		if err != nil {
 			return err
 		}
-
-		if balance < transaction.TotalAmount {
-			return fmt.Errorf("insufficient balance")
+		if !hasSufficientBalance(balance, transaction.TotalAmount) {
+			return fmt.Errorf("%s: available %.2f, required %.2f", ErrInsufficientBalance, balance, transaction.TotalAmount)
 		}
-
+		newBalance := balance - transaction.TotalAmount
 		_, err = tx.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
-			newBalance := balance - transaction.TotalAmount
 			pipe.HSet(ctx, accountKey, category, newBalance)
 			return nil
 		})
-
 		return err
-	})
+	}, accountKey)
 
 	if err != nil {
-		return fmt.Errorf("Transaction failed: %v", err)
+		return fmt.Errorf("transaction failed: %v", err)
 	}
 
 	return nil
+}
+
+func getMCCMapToCategory(mcc string) string {
+	switch mcc {
+	case MCCFood1, MCCFood2:
+		return "FOOD"
+	case MCCFood3, MCCFood4:
+		return "MEAL"
+	default:
+		return "CASH"
+	}
+}
+
+func getBalance(ctx context.Context, tx *redis.Tx, accountKey, category string) (float64, error) {
+	balanceStr, err := tx.HGet(ctx, accountKey, category).Result()
+	if err == redis.Nil {
+		return 0, fmt.Errorf("%s for account: %s, category: %s", ErrAccountNotExists, accountKey, category)
+	} else if err != nil {
+		return 0, err
+	}
+
+	balance, err := strconv.ParseFloat(balanceStr, 64)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to parse balance: %v", err)
+	}
+
+	return balance, nil
+}
+
+func hasSufficientBalance(balance, totalAmount float64) bool {
+	return balance >= totalAmount
 }
