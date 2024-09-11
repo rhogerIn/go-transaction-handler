630a166f4b7146d45ef1e19f7beb09f72f737bda
Rogério Souza <67068084+rhogerIn@users.noreply.github.com>
2024-09-11 06:58:14 -0300

    feat: Added pipeline transaction batch

--- internal/core/transaction.go
+++ internal/core/transaction.go
@@ -14,6 +14,10 @@ type Transaction struct {
 	Merchant    string  `json:"merchant"`
 }
 
+type TransactionBatch struct {
+	Transactions []Transaction
+}
+
 const (
 	MCCFood1 = "5411"
 	MCCFood2 = "5412"
@@ -24,6 +28,8 @@ const (
 	ErrInsufficientBalance = "Insufficient balance"
 )
 
+
+
 func HandleTransaction(ctx context.Context, rdb *redis.Client, transaction Transaction) error {
 	category := getMCCMapToCategory(transaction.Mcc)
 	if category == "" {
@@ -31,7 +37,7 @@ func HandleTransaction(ctx context.Context, rdb *redis.Client, transaction Trans
 	}
 	accountKey := "account:" + transaction.Account
 	err := rdb.Watch(ctx, func(tx *redis.Tx) error {
-		balance, err := getBalance(ctx, tx, accountKey, category)
+		balance, err := getBalance(ctx, rdb, accountKey, category)
 		if err != nil {
 			return err
 		}
@@ -64,17 +70,34 @@ func getMCCMapToCategory(mcc string) string {
 	}
 }
 
-func getBalance(ctx context.Context, tx *redis.Tx, accountKey, category string) (float64, error) {
-	balanceStr, err := tx.HGet(ctx, accountKey, category).Result()
+func getBalance(ctx context.Context, rdb *redis.Client, accountKey, category string) (float64, error) {
+	balanceStr, err := rdb.HGet(ctx, accountKey, category).Result()
 	if err == redis.Nil {
-		return 0, fmt.Errorf("%s for account: %s, category: %s", ErrAccountNotExists, accountKey, category)
+		return 0, fmt.Errorf("account or category does not exist")
 	} else if err != nil {
 		return 0, err
 	}
 
 	balance, err := strconv.ParseFloat(balanceStr, 64)
 	if err != nil {
-		return 0, fmt.Errorf("Failed to parse balance: %v", err)
+		return 0, fmt.Errorf("failed to parse balance: %v", err)
+	}
+
+	return balance, nil
+}
+
+func GetAccountBalance(ctx context.Context, rdb *redis.Client, account, category string) (float64, error) {
+	accountKey := "account:" + account
+	balanceStr, err := rdb.HGet(ctx, accountKey, category).Result() // Only catch if is UpperCase Category
+	if err == redis.Nil {
+		return 0, fmt.Errorf("account or category does not exist")
+	} else if err != nil {
+		return 0, err
+	}
+
+	balance, err := strconv.ParseFloat(balanceStr, 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse balance: %v", err)
 	}
 
 	return balance, nil
@@ -82,4 +105,61 @@ func getBalance(ctx context.Context, tx *redis.Tx, accountKey, category string) 
 
 func hasSufficientBalance(balance, totalAmount float64) bool {
 	return balance >= totalAmount
+}
+
+func Deposit(ctx context.Context, rdb *redis.Client, account, category string, amount float64) error {
+	accountKey := "account:" + account
+	balanceStr, err := rdb.HGet(ctx, accountKey, category).Result() // Only catch if is UpperCase Category
+	if err == redis.Nil {
+		return fmt.Errorf("account or category does not exist %v", err)
+	} else if err != nil {
+		return err
+	}
+
+	balance, err := strconv.ParseFloat(balanceStr, 64)
+	if err != nil {
+		return fmt.Errorf("failed to parse balance: %v", err)
+	}
+
+	newBalance := balance + amount
+	err = rdb.HSet(ctx, accountKey, category, newBalance).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ProcessTransactionBatch(ctx context.Context, rdb *redis.Client, batch TransactionBatch) error {
+	pipe := rdb.Pipeline()
+	results := make([]*redis.StringCmd, len(batch.Transactions))
+
+	for i, transaction := range batch.Transactions {
+		accountKey := "account:" + transaction.Account
+		results[i] = pipe.HGet(ctx, accountKey, getMCCMapToCategory(transaction.Mcc))
+	}
+
+	_, err := pipe.Exec(ctx)
+	if err != nil {
+		return fmt.Errorf("error executing pipeline: %v", err)
+	}
+
+	pipe = rdb.Pipeline()
+	for i, transaction := range batch.Transactions {
+		balanceStr := results[i].Val()
+		balance, err := strconv.ParseFloat(balanceStr, 64)
+		if err != nil {
+			return fmt.Errorf("failed to parse balance: %v", err)
+		}
+
+		newBalance := balance - transaction.TotalAmount
+		accountKey := "account:" + transaction.Account
+		pipe.HSet(ctx, accountKey, getMCCMapToCategory(transaction.Mcc), newBalance)
+	}
+
+	_, err = pipe.Exec(ctx)
+	if err != nil {
+		return fmt.Errorf("error executing pipeline: %v", err)
+	}
+
+	return nil
 }
